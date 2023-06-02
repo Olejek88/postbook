3d5fa3b97ca430699f8873ef0ae84900f49f4d5a
oleg.romanov <oleg.romanov@apprime.de>
2023-06-02 16:34:12 +0200

    Add app theme

--- app/src/main/java/de/olegrom/postbook/MainActivity.kt
+++ app/src/main/java/de/olegrom/postbook/MainActivity.kt
@@ -10,14 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import de.olegrom.postbook.ui.theme.PostbookTheme
+import de.olegrom.postbook.ui.theme.PostBookTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PostbookTheme {
-                // A surface container using the 'background' color from the theme
+            PostBookTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -37,7 +36,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    PostbookTheme {
+    PostBookTheme {
         Greeting("Android")
     }
 }
